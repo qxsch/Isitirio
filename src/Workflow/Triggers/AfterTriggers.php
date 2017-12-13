506eef6b0894039548c49815d3cffc65f3933981
qxsch <qxsch@users.noreply.github.com>
2017-12-13 02:18:34 +0100

    removed test methods

--- src/Workflow/Triggers/AfterTriggers.php
+++ src/Workflow/Triggers/AfterTriggers.php
@@ -5,7 +5,4 @@ namespace Isitirio\Workflow\Triggers;
 use Isitirio\Workflow\Events\EventInterface;
 
 class AfterTriggers {
-	public static function test(EventInterface $event, array $configuration) {
-echo "hi\n";
-	}
 }
