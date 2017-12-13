7743fbe68c6dc1d2fc10834732c3c69bbcced44c
qxsch <qxsch@users.noreply.github.com>
2017-12-13 02:21:21 +0100

    first workflow builder test

--- test.php
+++ test.php
@@ -1,66 +1,56 @@
 #!/usr/bin/env php
 <?php
 
+require_once('autoload.php');
 require_once('vendor/autoload.php');
-require_once('libs/IssueStatus.php');
-
-/*
-use Symfony\Component\Yaml\Yaml;
-print_r(
-	Yaml::parseFile('test.yaml')
-);
-*/
-
-use Symfony\Component\Workflow\DefinitionBuilder;
-use Symfony\Component\Workflow\Transition;
-use Symfony\Component\Workflow\Workflow;
-use Symfony\Component\Workflow\MarkingStore\SingleStateMarkingStore;
 
 
-$definitionBuilder = new DefinitionBuilder();
-$definition = $definitionBuilder->addPlaces(['draft', 'review', 'rejected', 'published'])
-	// Transitions are defined with a unique name, an origin place and a destination place
-	->addTransition(new Transition('to_review', 'draft', 'review'))
-	->addTransition(new Transition('publish', 'review', 'published'))
-	->addTransition(new Transition('reject', 'review', 'rejected'))
-	->build()
-;
-
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\EventDispatcher\Event;
-
-$dispatcher = new EventDispatcher();
+$yaml = Isitirio\Yaml::createFromArray(Symfony\Component\Yaml\Yaml::parseFile("test.yaml"));
 
 
-// more info on events:   https://symfony.com/doc/current/workflow/usage.html
-$dispatcher->addListener('workflow.Issue.guard', function (Event $event) {
-	//echo "GUARD status ". $event->getSubject()->getStatus() .".... ".get_class($event)."\n";
-	//echo "Workflow: ".$event->getWorkflowName()."\n";
-	//  $event->setBlocked(true);  // disable this transition
-});
-$dispatcher->addListener('workflow.Issue.leave', function (Event $event) {
-	echo "Leave status ". $event->getSubject()->getStatus() .".... ".get_class($event)."\n";
-	echo "Workflow: ".$event->getWorkflowName()."\n";
-	echo $event->getTransition()->getFroms()[0]  . " => " . $event->getTransition()->getTos()[0] ."\n";
-});
-$dispatcher->addListener('workflow.Issue.entered', function (Event $event) {
-	echo "ENTERED status ". $event->getSubject()->getStatus() .".... ".get_class($event)."\n";
-	echo "Workflow: ".$event->getWorkflowName()."\n";
-	echo $event->getTransition()->getFroms()[0]  . " => " . $event->getTransition()->getTos()[0] ."\n";
-});
+class test {
+	public static function inject(array $configuration) {
+		return new Isitirio\Workflow\Trigger(
+			function(Isitirio\Workflow\Events\EventInterface $event, array $configuration) {
+				echo "hi";
+			},
+			$configuration
+		);
+
+	}
+
+	public static function callMe(Isitirio\Workflow\Events\EventInterface $event, array $configuration) {
+		echo "called...\n";
+	}
+}
+/*echo $yaml->dump();
+echo $yaml->dump(true);
 
 
-$marking = new SingleStateMarkingStore('status');  // getStatus / setStatus Method  or status property
-$workflow = new Workflow($definition, $marking, $dispatcher, 'Issue');
+echo "---------\n";
+
+echo $yaml->get('Workflows.Monitoring Workflow.InitialState');
+echo "\n";
+
+echo $yaml['Workflows']['Monitoring Workflow']['InitialState'];
+echo "\n";
+
+foreach($yaml['Workflows'] as $key => $v) {
+	echo " - $key\n";
+}
+
+echo $yaml->get('Workflows.Monitoring Workflow.InitialState');
+
+
+echo "\n";*/
 
 
 
-$o=new Isitirio\Backend\IssueStatus('draft');
 
-echo "Can publish: "; var_dump($workflow->can($o, 'publish')); // False
-echo "Can to_review: "; var_dump($workflow->can($o, 'to_review')); // True
 
-$workflow->apply($o, 'to_review');
-echo "Can publish: "; var_dump($workflow->can($o, 'publish')); // True
-var_dump($workflow->getEnabledTransitions($o));;
+Isitirio\Workflow\WorkflowRegistryBuilder::buildFromYaml($yaml, 'Workflows');
+
+
+
+echo "\n";
 
